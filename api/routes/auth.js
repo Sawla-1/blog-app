c4bbb0b2016250bfa8dfe8c69cc95590b18f6f28
Sha Doh <ncboyboyboy@gmail.com>
2024-12-06 11:52:14 +0700

    Hash password with bcrypt also update some code

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,16 @@
 const router = require("express").Router(); // prepare to use express framework to create router
 const User = require("../models/User");     // prepare to use UserSchema
+const bcrypt = require("bcrypt");           // Prepare to use bcrypt to hash password
 
 //REGISTER
 router.post("/register", async (req,res) => {
     try{
+        const salt = await bcrypt.genSalt(10);                          //hash password 10 times
+        const hashedPass = await bcrypt.hash(req.body.password, salt);  // request user type in password
         const newUser = new User({          //>> use UserSchema in moldels folder
             username: req.body.username,    //>> Specific what the user will type or submit.
             email: req.body.email,          //      It means it will only take username, email 
-            password: req.body.password,    //      and password
+            password: hashedPass,           //      and password
         });
 
         const user = await newUser.save();  // To save this user. Use await since async process. This save method is coming from mongoose since we're using UserSchema
