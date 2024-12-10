f96982f674ede56ae5dd197c2b315221b6b29930
Sha Doh <ncboyboyboy@gmail.com>
2024-12-10 09:02:48 +0700

    Create Login Route(route component)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,10 +3,10 @@ const User = require("../models/User");     // prepare to use UserSchema
 const bcrypt = require("bcrypt");           // Prepare to use bcrypt to hash password
 
 //REGISTER
-router.post("/register", async (req,res) => {
+router.post("/register", async (req,res) => {   // create register router ("/register")
     try{
         const salt = await bcrypt.genSalt(10);                          //hash password 10 times
-        const hashedPass = await bcrypt.hash(req.body.password, salt);  // request user type in password
+        const hashedPass = await bcrypt.hash(req.body.password, salt);  // request user type in password and hash
         const newUser = new User({          //>> use UserSchema in moldels folder
             username: req.body.username,    //>> Specific what the user will type or submit.
             email: req.body.email,          //      It means it will only take username, email 
@@ -22,5 +22,19 @@ router.post("/register", async (req,res) => {
 })
 
 //LOGIN
+router.post("/login", async (req,res) => {  // create login router ("/login")
+    try {
+        const user = await User.findOne({ username: req.body.username });   // use.findOne() method to find user in mongoDB
+        !user && res.status(400).json("Wrong credentials!");                // if user not found, respond "wrong credentials!"
+
+        const validated = await bcrypt.compare(req.body.password, user.password); // use .compare() to compare compare passwords
+        !validated && res.status(400).json("Wrong credentials!");   // if passwords are not match, respond "wrong credentials!"
+
+        const { password, ...others} = user._doc;                   // store user info in variables using destructuring and spread operator
+        res.status(200).json(others);                               // when process successful, respond user info without password
+    } catch (err) {
+        res.status(500).json(err);                                  // if can't go to route, respond error
+    }
+})
 
 module.exports = router // export router
