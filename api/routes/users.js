268fe0c956be2bb243368ac775f84b8ad625a602
Sha Doh <ncboyboyboy@gmail.com>
2024-12-10 11:43:15 +0700

    Create DELETE Route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router(); // prepare to use express framework to create router
 const User = require("../models/User");     // prepare to use UserSchema
+const Post = require("../models/Post");     // prepare to use UserSchema
 const bcrypt = require("bcrypt");           // Prepare to use bcrypt to hash password
 
 //UPDATE
@@ -28,5 +29,32 @@ router.put("/:id", async (req,res) => {   // create register router ("/:id")
 });
 
 // DELETE
+router.delete("/:id", async (req,res) => {   // create register router ("/:id")
+    if(req.body.userId === req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({username: user.username});
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("User has been deleted...")
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } catch(err){
+            res.status(404).json("User not found!");          // send error respond
+        }
+    }  else{
+        res.status(401).json("You can delete only your account!");
+    }
+});
+
+// GET
+router.get("/:id", async (req,res) => {   // create register router ("/:id")
+    try{
+        
+    } catch(err){
+        res.status(404).json("User not found!");          // send error respond
+    }
+});
 
 module.exports = router // export router
