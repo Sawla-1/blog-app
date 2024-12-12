bc67c707b4ebaec509c496625e66ca2827e40803
Sha Doh <ncboyboyboy@gmail.com>
2024-12-12 08:03:32 +0700

    GET all Posts(posts.js)

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -70,4 +70,28 @@ router.get("/:id", async (req,res) => {   // create get post router ("/:id")
     }
 });
 
+// GET All Posts
+router.get("/", async (req,res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if(username){
+            posts = await Post.find({ username });
+        }else if(catName){
+            posts = await Post.find({ 
+                categories:{
+                    $in: [catName],
+                },
+            });
+        }else{
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
 module.exports = router // export router
