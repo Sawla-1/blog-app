d30ff028782366036aa24f4a21077670bfb69d8b
Sha Doh <ncboyboyboy@gmail.com>
2024-12-18 15:11:32 +0700

    Fetch posts data from mongodb with axios and display on singlePost page

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,34 +1,44 @@
+import { useLocation } from "react-router-dom";
 import "./singlePost.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function SinglePost() {
+
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post,setPost] = useState({});
+
+  useEffect(()=>{
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  },[path])
+
   return (
     <div className='singlePost'>
       <div className="singlePostWrapper">
-        <img className="singlePostImg" src="https://i.pinimg.com/736x/e5/2a/7c/e52a7c54f203f3ab593f79c32099dd07.jpg" alt="" />
+        {post.photo && (
+          <img 
+            className="singlePostImg" 
+            src={post.photo} 
+            alt="" />
+        )}
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor sit amet consectetur
+          {post.title}
           <div className="singlePostEdit">
           <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
           <i className="singlePostIcon fa-solid fa-trash"></i>
           </div>
         </h1>
         <div className="singlePostInfo">
-          <span className="singlepostAuthor">Author: <b>Safak</b></span>
-          <span className="singlepostDate">1 hour ago</span>
+          <span className="singlepostAuthor">Author: <b>{post.username}</b></span>
+          <span className="singlepostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ex dolor ad.
-           Maxime cum veniam unde totam laudantium recusandae, voluptatum porro et sapiente
-            alias, magnam impedit dolores maiores debitis sed.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ex dolor ad.
-           Maxime cum veniam unde totam laudantium recusandae, voluptatum porro et sapiente
-            alias, magnam impedit dolores maiores debitis sed.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ex dolor ad.
-           Maxime cum veniam unde totam laudantium recusandae, voluptatum porro et sapiente
-            alias, magnam impedit dolores maiores debitis sed.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ex dolor ad.
-           Maxime cum veniam unde totam laudantium recusandae, voluptatum porro et sapiente
-            alias, magnam impedit dolores maiores debitis sed.
+          {post.desc}
         </p>
       </div>
     </div>
