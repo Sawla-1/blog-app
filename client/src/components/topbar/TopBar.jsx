c5a20e2bf54626784befd883281c80259466992a
Sha Doh <ncboyboyboy@gmail.com>
2024-12-24 10:05:56 +0700

    Login system and Logout system , Login Logout change pages and nav bar

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom"
 import "./topbar.css"
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 export default function TopBar() {
-  const user = false;
+  const {user, dispatch} = useContext(Context);
+
+  const handleLogout = ()=>{
+    dispatch({type: "LOGOUT"});
+  };
+
   return (
     <div className="top">
       <div className="topLeft">
@@ -17,13 +24,13 @@ export default function TopBar() {
             <li className="topListItem"><Link className="link" to="/about" >ABOUT</Link></li>
             <li className="topListItem"><Link className="link" to="/contact" >CONTACT</Link></li>
             <li className="topListItem"><Link className="link" to="/write" >WRITE</Link></li>
-            <li className="topListItem">{user && "LOGOUT"}</li>
+            <li className="topListItem" onClick={handleLogout}>{user && "LOGOUT"}</li>
         </ul>
       </div>
       <div className="topRight">
         {
           user? (
-            <img className="topImg" src="https://scontent.fphs2-1.fna.fbcdn.net/v/t39.30808-1/438255949_1256588621988739_1254776741703787855_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=7UPY2u8D3vwQ7kNvgENI_Mj&_nc_zt=24&_nc_ht=scontent.fphs2-1.fna&_nc_gid=ALjPZ9N8C345NAi6J9-8k97&oh=00_AYB8zm0vaOBxYZhEbJCdturJpRd3Q2YtOtUrTZBB0U5tFg&oe=67659F75" alt="" />
+            <img className="topImg" src={user.profilePic} alt="" />
           ) : (
             <ul className="topList">
               <li className="topListItem"><Link className="link" to="/login" >LOGIN</Link></li>
